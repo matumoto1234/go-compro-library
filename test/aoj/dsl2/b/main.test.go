d50df21aafe8aa6fcae099b16891bf08b94a1611
matumoto1234 <matumotosizuoka@gmail.com>
2023-04-14 19:40:33 +0900

    add segment tree test

---
+++ test/aoj/dsl2/b/main.test.go
@@ -0,0 +1,43 @@
+// verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/matumoto1234/go-compro-library/data_structure"
+	"github.com/matumoto1234/go-compro-library/utility"
+)
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	stdout := bufio.NewWriter(os.Stdout)
+	defer stdout.Flush()
+
+	var n, q int
+	fmt.Fscan(stdin, &n, &q)
+
+	st := data_structure.NewSegmentTree(n, utility.NewMonoid(
+		func(a, b int) int {
+			return a + b
+		},
+		func() int {
+			return 0
+		},
+	))
+
+	for i := 0; i < q; i++ {
+		var com, x, y int
+		fmt.Fscan(stdin, &com, &x, &y)
+		x--
+
+		switch com {
+		case 0:
+			st.Set(x, st.Get(x)+y)
+		case 1:
+			fmt.Fprintln(stdout, st.Prod(x, y))
+		}
+	}
+}
